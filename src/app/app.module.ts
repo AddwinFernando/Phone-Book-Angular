dabba9e1ace815e1e03274304e9a4f67827b5292
AddwinFernando <addwinfernando.jacob@kumaran.com>
2023-10-19 12:31:10 +0530

    Added About Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { Test2Component } from './components/test2/test2.component';
 import{TestService} from './service/test.service';
 import { TodoComponent } from './components/todo/todo.component';
 import { FormsModule } from '@angular/forms';
+import { AboutProjectComponent } from './components/about-project/about-project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Test1Component,
     Test2Component,
-    TodoComponent
+    TodoComponent,
+    AboutProjectComponent
   ],
   imports: [
     BrowserModule,
