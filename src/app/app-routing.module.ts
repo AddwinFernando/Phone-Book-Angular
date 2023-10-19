20d41c419ac9a231676724b98a2f385c4b1f2be7
AddwinFernando <addwinfernando.jacob@kumaran.com>
2023-10-19 12:58:45 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoComponent } from './components/todo/todo.component';
+import { AboutProjectComponent } from './components/about-project/about-project.component';
 
-const routes: Routes = [];
-
+ const routes: Routes = [
+    {path:"",component:AboutProjectComponent},
+    {path:"home",component:TodoComponent},
+  ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
